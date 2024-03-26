5452f20ccc446a775c3faf529ee69e8345c9cb6c
Raúl Armas <raularmasbx@gmail.com>
2024-03-25 23:34:12 -0500

    parametrizacion de variables de entorno

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore/lite';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlVhzlku5x_ZiwYwR0IY1axpLjiT1iwtk",
-  authDomain: "app-journal-58fc3.firebaseapp.com",
-  projectId: "app-journal-58fc3",
-  storageBucket: "app-journal-58fc3.appspot.com",
-  messagingSenderId: "413266384903",
-  appId: "1:413266384903:web:4a76909ef037e265ebf2a2",
-  measurementId: "G-XZL2J8HH98"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
