bf790a902fd843cf174afbe39f84d0f97c6f6619
Raúl Armas <raularmasbx@gmail.com>
2025-04-25 20:56:41 -0500

    update

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,3 +1,10 @@
-export const getEnvironments = {
-    ...import.meta.env
-  };
+export const getEnvironments = () => ({
+  VITE_APIKEY: import.meta.env.VITE_APIKEY,
+  VITE_AUTHDOMAIN: import.meta.env.VITE_AUTHDOMAIN,
+  VITE_DATABASEURL: import.meta.env.VITE_DATABASEURL,
+  VITE_PROJECTID: import.meta.env.VITE_PROJECTID,
+  VITE_STORAGEBUCKET: import.meta.env.VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID: import.meta.env.VITE_MESSAGINGSENDERID,
+  VITE_APPID: import.meta.env.VITE_APPID,
+  VITE_CLOUD_URL_BINARY: import.meta.env.VITE_CLOUD_URL_BINARY
+});
